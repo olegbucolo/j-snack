44cbb3c8bfebf89929a99f4b9c7d1f7b5474aa22
olegbucolo <olegbucolo97@gmail.com>
2025-12-04 11:26:51 +0100

    finito il j-snack

---
+++ jSnack.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt('inserisci numero 1'));
+let num2 = parseFloat(prompt('inserisci numero 2'));
+if(num1 > num2){
+    console.log(`il numero maggiore e':`, num1)
+}else if(num2 > num1){
+    console.log(`il numero maggiore e':`, num2)
+}else{
+    console.log(`i numeri sono uguali`)
+}
